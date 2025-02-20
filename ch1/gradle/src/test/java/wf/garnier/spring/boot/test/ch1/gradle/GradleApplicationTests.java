74b0a883fc8e17d7cae492fbd4653e79c6783df5
Daniel Garnier-Moiroux <git@garnier.wf>
2025-02-20 09:54:40 +0100

    ch1: add empty maven and Gradle projects

---
+++ ch1/gradle/src/test/java/wf/garnier/spring/boot/test/ch1/gradle/GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.spring.boot.test.ch1.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ch1/maven/src/test/java/wf/garnier/spring/boot/test/ch1/maven/MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.spring.boot.test.ch1.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
