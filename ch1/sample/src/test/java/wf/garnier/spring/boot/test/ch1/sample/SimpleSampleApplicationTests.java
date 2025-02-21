60b9cf5d560da10ad2694c083a7ba9a4c3f66a90
Daniel Garnier-Moiroux <git@garnier.wf>
2025-02-21 09:40:31 +0100

    ch1: add empty sample project

---
+++ ch1/sample/src/test/java/wf/garnier/spring/boot/test/ch1/sample/SimpleSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.spring.boot.test.ch1.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
