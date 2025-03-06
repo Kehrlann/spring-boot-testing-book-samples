d00f157abc8c84e688b205782316d2ee46d3390f
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-06 14:53:37 +0100

    ch2: rename WeatherAppApplication to WeatherApplication

--- ch2/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/weather/WeatherAppplication.java
+++ ch2/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/weather/WeatherAppplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherAppApplication {
+public class WeatherAppplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherAppApplication.class, args);
+		SpringApplication.run(WeatherAppplication.class, args);
 	}
 
 }
