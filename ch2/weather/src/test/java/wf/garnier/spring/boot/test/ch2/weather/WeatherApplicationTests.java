9a444a3acb69ce3fcd2c863a92b827f46f1f6d0f
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-08 13:14:05 +0100

    ch2: add contextLoads test

---
+++ ch2/weather/src/test/java/wf/garnier/spring/boot/test/ch2/weather/WeatherApplicationTests.java
@@ -0,0 +1,15 @@
+package wf.garnier.spring.boot.test.ch2.weather;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
