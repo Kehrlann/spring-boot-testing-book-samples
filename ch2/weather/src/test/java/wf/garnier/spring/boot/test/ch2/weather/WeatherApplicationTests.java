7d1ea940866a437e5fa259d2fd10e61660fd1dae
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-10 17:56:40 +0100

    ch2: add test

--- ch2/weather/src/test/java/wf/garnier/spring/boot/test/ch2/weather/WeatherApplicationTests.java
+++ ch2/weather/src/test/java/wf/garnier/spring/boot/test/ch2/weather/WeatherApplicationTests.java
@@ -2,14 +2,33 @@ package wf.garnier.spring.boot.test.ch2.weather;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WeatherApplicationTests {
+
+	@Autowired
+	private MockMvcTester mvc;
 
 	@Test
 	void contextLoads() {
 
 	}
 
+	@Test
+	void indexPageLoads() {
+		var resp = mvc.get()
+				.uri("/")
+				.exchange();
+		assertThat(resp)
+				.hasStatus(HttpStatus.OK)
+				.bodyText()
+				.contains("<h1>Weather App</h1>");
+	}
 }
