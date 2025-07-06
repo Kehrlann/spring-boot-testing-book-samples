965e1091d387031f20ce74010fc0513c4445f5e7
Daniel Garnier-Moiroux <git@garnier.wf>
2025-07-06 16:32:40 +0200

    ch2: fix application class name

--- ch2/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/WeatherApplication.java
+++ ch2/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/WeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherAppplication {
+public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherAppplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args);
 	}
 
 }
