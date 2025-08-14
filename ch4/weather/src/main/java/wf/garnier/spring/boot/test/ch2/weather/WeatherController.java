eebd62f26cc55ef4ccf8c1577be6fbd7839acc81
Daniel Garnier-Moiroux <git@garnier.wf>
2025-08-14 21:03:08 +0200

    ch4: remove unused non-/api endpoints

--- ch4/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/WeatherController.java
+++ ch4/weather/src/main/java/wf/garnier/spring/boot/test/ch2/weather/WeatherController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,31 +39,19 @@ public class WeatherController {
 		return selectionService.getWeatherInSelectedCities();
 	}
 
-	@PostMapping(value = "/city/add-by-id")
-	public String addCityById(long cityId) {
-		selectionService.addCityById(cityId);
-		return "redirect:/";
-	}
-
 	@PostMapping(value = "/api/city")
 	public ResponseEntity<String> addCityApi(@RequestBody CityRequest req) {
 		return selectionService.addCityById(req.id()) ? ResponseEntity.status(HttpStatus.CREATED).build()
 				: ResponseEntity.status(HttpStatus.CONFLICT).body("City already selected");
 	}
 
-	public record CityRequest(long id) {
-	}
-
-	@PostMapping(value = "/city/delete/{id}")
-	public String addCity(@PathVariable long id) {
-		selectionService.unselectCityById(id);
-		return "redirect:/";
-	}
-
 	@DeleteMapping(value = "/api/city")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteCity(@RequestBody CityRequest req) {
 		selectionService.unselectCityById(req.id());
+	}
+
+	public record CityRequest(long id) {
 	}
 
 }
