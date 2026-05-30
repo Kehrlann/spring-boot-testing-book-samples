7bc737a9230975f338d1e3daf32af325f4ec60ad
Daniel Garnier-Moiroux <git@garnier.wf>
2026-05-30 22:47:08 +0200

    ch5: autoconfiguration import

---
+++ ch5/weather/src/test/java/wf/garnier/spring/boot/test/ch5/weather/WebMvcTestUserAutoConfiguration.java
@@ -0,0 +1,28 @@
+package wf.garnier.spring.boot.test.ch5.weather;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * This configuration will be loaded in all {@link WebMvcTest}-based tests, because it is
+ * referenced in the
+ * {@code wf.garnier.spring.boot.test.ch5.weather.WebMvcTestUserAutoConfiguration} file in
+ * {@code META-INF/spring}.
+ */
+class WebMvcTestUserAutoConfiguration {
+
+	private static final Logger log = LoggerFactory.getLogger(WebMvcTestUserAutoConfiguration.class);
+
+	@Bean
+	ApplicationRunner applicationRunner() {
+		return args -> {
+			log.info("🧪🧪 Hello for user-provided test autoconfiguration. This log line comes from {}.",
+					this.getClass().getSimpleName());
+		};
+	}
+
+}
